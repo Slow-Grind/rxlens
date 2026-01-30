fa77fb0a0b3189ad55e307b29a03f8777fdaea6e
slowgrind <tankionline4654@gmail.com>
2026-01-30 18:23:49 +0500

    feat(app): added logo

---
+++ src/app/pages/main-page/main-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rxl-main-page',
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export class MainPage {}
--- src/app/widgets/main-header/ui/main-header.ts
+++ src/app/widgets/main-header/ui/main-header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Logo } from '../../../shared/ui/logo/logo';
 
 @Component({
   selector: 'rxl-main-header',
-  imports: [],
+  imports: [Logo],
   templateUrl: './main-header.html',
   styleUrl: './main-header.scss',
 })
-export class MainHeader {
-
-}
+export class MainHeader {}
